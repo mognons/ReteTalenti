bd5f8d08d77e5df5252509693ad8aa7662563ad3
mognons <alberto.mognoni@gmail.com>
2015-12-15 09:58:50 +0100

    Fixed JAR conflict

--- ReteTalenti/src/com/listener/ReteTalentiContextListener.java
+++ ReteTalenti/src/com/listener/ReteTalentiContextListener.java
@@ -7,20 +7,18 @@ import java.util.Enumeration;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import org.apache.log4j.Logger;
 
 import com.action.LogoutAction;
 import com.jdbc.DataAccessObject;
  
 public class ReteTalentiContextListener 
                implements ServletContextListener{
-	static final Logger LOGGER = Logger.getLogger(ReteTalentiContextListener.class);
 
 	@Override
 	public final void contextDestroyed(ServletContextEvent sce) {
 		
 		// Close Database Connection
-		LOGGER.info("Closing DB Connection");
+		System.out.println("Closing DB Connection");
 	    DataAccessObject.closeConnection();
 	    
 		// Now deregister JDBC drivers in this context's ClassLoader:
@@ -33,20 +31,20 @@ public class ReteTalentiContextListener
 	        if (driver.getClass().getClassLoader() == cl) {
 	            // This driver was registered by the webapp's ClassLoader, so deregister it:
 	            try {
-	                LOGGER.info("Deregistering JDBC driver " + driver);
+	                System.out.println("Deregistering JDBC driver " + driver);
 	                DriverManager.deregisterDriver(driver);
 	            } catch (SQLException ex) {
-	                LOGGER.error("Error deregistering JDBC driver " + driver);
+	                System.err.println("Error deregistering JDBC driver " + driver);
 	            }
 	        } else {
 	            // driver was not registered by the webapp's ClassLoader and may be in use elsewhere
-                LOGGER.info("JDBC driver not registered with this app " + driver);
+                System.out.println("JDBC driver not registered with this app " + driver);
 	        }
 	    }
 	} 
         //Run this before web application is started
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
-		LOGGER.info("Application ReteTalenti starting...");
+		System.out.println("Application ReteTalenti starting...");
 	}
 }
